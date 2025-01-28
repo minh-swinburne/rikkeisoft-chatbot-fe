85d2491cd4ad674fd464375fa6a33f2d3de685fc
Minh <104169617@student.swin.edu.au>
2025-01-29 00:40:27 +0700

    fix(sso): correct google config

--- src/plugins/sso/googleConfig.js
+++ src/plugins/sso/googleConfig.js
@@ -1,3 +1,3 @@
 export default {
-  clienId: '1047088098330-2d17mgbf5bdugkvkh69i0ah65c40hp65.apps.googleusercontent.com',
+  clientId: '1047088098330-2d17mgbf5bdugkvkh69i0ah65c40hp65.apps.googleusercontent.com',
 }
